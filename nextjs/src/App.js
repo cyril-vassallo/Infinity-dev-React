dcfb1adf525f6aea7f88b489888c0e0a2a45a84b
zhangsaili <cyrilvssll34@gmail.com>
2020-08-14 20:22:47 +0200

    remove import in App from next

--- nextjs/src/App.js
+++ nextjs/src/App.js
@@ -1,26 +1,12 @@
-import React from "react";  
-//import { Route, Switch } from "react-router-dom";
+import React from "react";
+
 import Portfolio from "./pages/portfolio/Portfolio";
-import Downloads from "./pages/downloads/Downloads";
-import Demo from "./pages/demo/Demo"
-
-
-
-
 
 function App() {
   return (
-
-      <div className="App" id="app">  
-      <Portfolio/>     
-        {/* <Switch>
-          <Route path="/downloads" component={Downloads} />
-          <Route path="/demo" component={Demo} />
-          <Route path="/" component={Portfolio} />
-        </Switch> */}
-      </div>
-
-
+    <div className="App" id="app">
+      <Portfolio />
+    </div>
   );
 }
 
